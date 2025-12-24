85c3a458584f1a95a88444bf22970d80220f891b
Yacine Cheikhrouhou <yacine.cheikhrouhou@yahoo.fr>
2025-12-24 06:43:10 +0100

    disable the svelte app

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
 
-import Counter from './lib/Counter.svelte'
-
+/* customElement exports */
+//import Counter from './lib/Counter.svelte'
+import Palette from './lib/Palette.svelte'
+import PaletteShow from './lib/PaletteShow.svelte';
 
 
 // comment this to disable the svelte app and only leave web components
 // the vite autoreload is useful for development though
-const app = mount(App, {
+/*const app = mount(App, {
   target: document.getElementById('app')!,
-})
+})*/
 
-export default app
+//export default app
